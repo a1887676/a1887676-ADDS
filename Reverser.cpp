3fa52182dbef93e02c5cc6376d20a07e3eb9c55f
a1887676 <a1887676@adelaide.edu.au>
2025-03-23 17:05:29 +1030

    completed reverseString

--- Reverser.cpp
+++ Reverser.cpp
@@ -9,9 +9,21 @@ int Reverser::reverseDigit(int value) {
 
     int endDigit = value % 10;
     int otherNums = value / 10;
+    int length = std::to_string(otherNums).length();
 
+    int posMultiplier = std::pow(10,length);
 
+    return endDigit*posMultiplier+reverseDigit(otherNums);
+ 
+}
 
+std::string Reverser::reverseString(std::string characters) {
+    if(characters.length()<=1) {
+        return characters;
+    }
 
+    std::string otherString = characters.substr(0,characters.length()-1);
+
+    return characters.back() + reverseString(otherString);
 
 }

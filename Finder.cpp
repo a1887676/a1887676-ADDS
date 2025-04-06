1977384de0e0126067b8f6bf5cfc2ddbabc42233
a1887676 <a1887676@adelaide.edu.au>
2025-04-06 19:42:41 +0930

    added implementation

--- Finder.cpp
+++ Finder.cpp
@@ -5,14 +5,29 @@ using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
 
     vector<int> result;
+    int index = 0;
+    bool endLarger=false;
+    string s2Substring = "";
 
     for(size_t i = 1; i <= s2.size(); i++) {
-        size_t found = s1.find(s2.substr(0, i));
+        if(endLarger == true){
+            result.push_back(-1);
+            continue;
+
+        }
+
+        s2Substring = s2Substring+s2[i];
+        size_t found = s1.find(s2Substring, index);
+        
         if (found != string::npos) {
             result.push_back(found);
+            index = found;
+        
         } else {
+            endLarger=true;
             result.push_back(-1);
         }
     }
+    
     return result;
 }
